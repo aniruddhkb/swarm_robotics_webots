10dc5e793513d6135a39be02d38d904d49276b35
aniruddhkb <aniruddhkb@gmail.com>
2020-07-05 16:45:47 +0530

    Refactored swarm_basic_flocking.py

--- controllers/swarm_basic_flocking/swarm_basic_flocking.py
+++ controllers/swarm_basic_flocking/swarm_basic_flocking.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3.6
 '''
-NOTE: There are several places where 'grapher' has been disabled. This is to save on computing 
-power, and these 'grapher' nodes are only necessary for data visualization.
+This is the main controller for swarming behaviour of ChuhaLidarCamera.
 
-NOTE2: The 'Keyboard' module is for debugging the LIDAR system by teleoperating the robot. When 
-using the Keyboard for teleoperation, disable the regular function of the code by commenting it 
-out.
+To modify the swarming behaviour, change the swarm_control function.
 
-I will have to refactor all this at some stage.
+The flow of code is as follows:
+1) Enable all relevant devices and initialize global parameters.
+2) Read and filter the LIDAR data
+3) Categorize the LIDAR data
+4) Obtain the positions of the neighbours from the LIDAR data
+5) Swarm control
 
+There is an optional graphing module which writes to the extra Display device provided.
+The functionality to write to this Display is handled through the get_neighbours function.
 
+There is an optional teleoperational module. To use it, comment out the call to swarm_control and
+uncomment all the keyboard-related lines.
 '''
 from controller import Robot, Motor, Lidar, Display, Keyboard
 import numpy as np
@@ -62,6 +68,13 @@ class Grapher:
         # self.drawPointCenter(0, 0, color=0xFF0000)
         self.drawPointsListCenter(x_s, y_s, size=size,color=color)
 def lidar_filter(imageArray, SIZES,RANGES,EPSILON):
+    '''
+    To extract the detected shapes from the raw LIDAR data. This is done by comparing 
+    the detected range with the default detected range for that layer when no object is there.
+    To make the detection more strict, increase EPSILON. This will decrease false negatives but 
+    will also decrease the detection range. To increase the detection range, decrease EPSILON.
+    This may introduce more noise.
+    '''
     theta_data = np.zeros((SIZES[1],)).tolist()
     for layer in range(SIZES[0]):
         for theta in range(SIZES[1]):
@@ -69,60 +82,26 @@ def lidar_filter(imageArray, SIZES,RANGES,EPSILON):
                 if(point_range < RANGES[layer]*(EPSILON)):
                     theta_data[theta] = point_range
     return theta_data
-
-#INITIALIZATION CODE HERE.
-VELOCITY = 60
-
-#ROBOT INITIALIZATION
-robot = Robot()
-timestep = int(robot.getBasicTimeStep())
-
-#LIDAR AND PLOTTING INITIALIZATION
-SIZES = (16, 512)
-ranges_str = "1.13114178 0.85820043 0.57785118 0.43461093 0.38639969 0.31585345 0.2667459 0.23062678 0.21593061 0.19141567 0.17178488 0.15571462 0.14872716 0.13643947 0.12597121 0.11696267"
-RANGES = [float(i) for i in ranges_str.split(' ')]
-EPSILON = 0.6
-DISPLAY_SIZE = (1024, 1024)
-DISPLAY_SCALING_FACTOR = 1024/RANGES[0]
-lidar = robot.getLidar("lidar")
-lidar.enable(timestep)
-# display = robot.getDisplay("extra_display")
-# grapher = Grapher(display)
-
-#MOTOR INITIALIZATION
-left_motor = robot.getMotor("left motor")
-right_motor = robot.getMotor("right motor")
-
-left_motor.setPosition(float('inf'))
-right_motor.setPosition(float('inf'))
-left_motor.setVelocity(0)
-right_motor.setVelocity(0)
-#KEYBOARD -- TEMPORARY
-# keyboard = Keyboard()
-# keyboard.enable(timestep)
-# print("Select the 3D window and control the robot using the W, A, S, D keys.")
-
-#NEIGHBOURS DETECTION INITIALIZATION
-DELTA_THETA = 0.1
-DELTA_R = 0.02
-# colors = [0xFF0000, 0x800000, 0xFFFF00, 0x808000, 0x00FF00,0x008000, 0x00FFFF, 0x008080, 0x0000FF, 0x000080, 0xFF00FF, 0x800080]
-
-#SWARM CONTROL INITIALIZATION
-LINEAR_FACTOR = 0.8*VELOCITY
-ANGULAR_FACTOR = 0.2*VELOCITY/np.pi
-while robot.step(timestep) != -1:
-
-    #ACQUIRING, FILTERING AND ALIGNING LIDAR DATA
+def get_theta_data_aligned(lidar, SIZES, RANGES, EPSILON):
+    '''
+    To get the LIDAR data in terms of right-handed r, theta. The robot's motion is 
+    along the y axis but the LIDAR data's 'zero' of theta is at the +ve X axis.
+    '''
     imageArray = np.array(lidar.getRangeImageArray()).T
     theta_data = lidar_filter(imageArray, SIZES,RANGES,EPSILON)
     theta_data_aligned = []
     for theta in range(len(theta_data)):
-        new_theta =(+np.pi/2 -  2*np.pi*theta/512)
+        new_theta =(+np.pi/2 -  2*np.pi*theta/SIZES[1])
         if(theta_data[theta] != 0):  
             theta_data_aligned.append((new_theta, theta_data[theta]))
-
-    #CATEGORIZING LIDAR DATA
-    color_index = 0
+    return theta_data_aligned
+def get_theta_data_colored(theta_data_aligned, DELTA_THETA, DELTA_R):
+    '''
+    To categorize the data points on the basis of which robot they belong to. The logic being
+    that points which are very close to each other are likely to belong to the same robot.
+    If two robots are being detected where there ought to be one, increase DELTA_THETA or DELTA_R.
+    If one robot is being detected where there are two, decrease DELTA_THETA or DELTA_R.
+    '''
     theta_data_colored = [[]]
     theta_prev = None 
     r_prev = None
@@ -140,11 +119,16 @@ while robot.step(timestep) != -1:
         if(abs(theta_prev - theta - 2*np.pi) < DELTA_THETA and abs(r_prev - r) < DELTA_R/r):
             last_category = theta_data_colored.pop()
             theta_data_colored[0].extend(last_category)
-    #PLOTTING LIDAR DATA
-    # color_index = 0
-    # grapher.clear()
+    return theta_data_colored
+def get_neighbours(theta_data_colored,DISPLAY_SCALING_FACTOR=None,colors=None, grapher=None, shouldGraph=False):
+    '''
+    To get the (x,y) of the neighbours of a robot (and, optionally, to plot to the relevant grapher).
+    '''
     neighbours_positions_x = []
     neighbours_positions_y = []
+    if(shouldGraph):
+        grapher.clear()
+    color_index = 0
     for obstacle in theta_data_colored:
         if(len(obstacle) > 0):
             #PLOTTING ALL POINTS
@@ -153,40 +137,98 @@ while robot.step(timestep) != -1:
             for theta, r in obstacle:
                 x_data.append(r*np.cos(theta))
                 y_data.append(r*np.sin(theta))
-            # grapher.drawPointCenter(0, 0, size=5, color=0xFFFFFF)
-            # grapher.lidar_plot_without_clearing(x_data, y_data, DISPLAY_SCALING_FACTOR, color=colors[color_index])
-            
             x_mean = sum(x_data)/len(x_data)
             y_mean = sum(y_data)/len(y_data)
             neighbours_positions_x.append(x_mean)
             neighbours_positions_y.append(y_mean)
-            # color_index = (color_index + 1)%len(colors)
-    # grapher.lidar_plot_without_clearing(neighbours_positions_x, neighbours_positions_y,DISPLAY_SCALING_FACTOR, size=10, color=0x808080)
-    #KEYBOARD -- TEMPORARY
-    # ascii_val = keyboard.getKey()
-    # if(ascii_val == -1):
-    #     left_motor.setVelocity(0)
-    #     right_motor.setVelocity(0)
-    # else:
-    #     key = chr(ascii_val).lower()
-    #     if(key == 'w'):
-    #         left_motor.setVelocity(VELOCITY)
-    #         right_motor.setVelocity(VELOCITY)
-    #     elif(key == 's'):
-    #         left_motor.setVelocity(-VELOCITY)
-    #         right_motor.setVelocity(-VELOCITY)
-    #     elif(key == 'a'):
-    #         left_motor.setVelocity(-VELOCITY)
-    #         right_motor.setVelocity(VELOCITY)
-    #     elif(key == 'd'):
-    #         left_motor.setVelocity(VELOCITY)
-    #         right_motor.setVelocity(-VELOCITY)
-
-    #CONTROL OF ROBOT BASED ON POSITIONS OF NEIGHBOURS
+            if(shouldGraph):
+                grapher.drawPointCenter(0, 0, size=5, color=0xFFFFFF)
+                grapher.lidar_plot_without_clearing(x_data, y_data, DISPLAY_SCALING_FACTOR, color=colors[color_index])
+                color_index = (color_index + 1)%len(colors)
+    if(shouldGraph):
+        grapher.lidar_plot_without_clearing(neighbours_positions_x, neighbours_positions_y,DISPLAY_SCALING_FACTOR, size=10, color=0x808080)    
+    return neighbours_positions_x, neighbours_positions_y
+def swarm_control(neighbours_positions_x, neighbours_positions_y, left_motor, right_motor, VELOCITY):
+    '''
+    The swarming behaviour is driven through this function. Modify it for different behaviour.
+    '''
+    LINEAR_FACTOR = 0.8*VELOCITY
+    ANGULAR_FACTOR = 0.2*VELOCITY/np.pi
     if(len(neighbours_positions_x) > 0):
-        mean_x = sum(neighbours_positions_x)/len(neighbours_positions_x)
-        mean_y = sum(neighbours_positions_y)/len(neighbours_positions_y)
-        angle = np.arctan2(mean_x, mean_y)
-        distance = np.sqrt(mean_x**2 + mean_y**2)
-        left_motor.setVelocity(distance*LINEAR_FACTOR + angle*ANGULAR_FACTOR)
-        right_motor.setVelocity(distance*LINEAR_FACTOR - angle*ANGULAR_FACTOR)
+            mean_x = sum(neighbours_positions_x)/len(neighbours_positions_x)
+            mean_y = sum(neighbours_positions_y)/len(neighbours_positions_y)
+            angle = np.arctan2(mean_x, mean_y)
+            distance = np.sqrt(mean_x**2 + mean_y**2)
+            left_motor.setVelocity(distance*LINEAR_FACTOR + angle*ANGULAR_FACTOR)
+            right_motor.setVelocity(distance*LINEAR_FACTOR - angle*ANGULAR_FACTOR)
+def teleoperation(keyboard, left_motor, right_motor, VELOCITY):
+    '''
+    To teleoperate the robot using the keyboard's WASD keys.
+    '''
+    ascii_val = keyboard.getKey()
+    if(ascii_val == -1):
+        left_motor.setVelocity(0)
+        right_motor.setVelocity(0)
+    else:
+        key = chr(ascii_val).lower()
+        if(key == 'w'):
+            left_motor.setVelocity(VELOCITY)
+            right_motor.setVelocity(VELOCITY)
+        elif(key == 's'):
+            left_motor.setVelocity(-VELOCITY)
+            right_motor.setVelocity(-VELOCITY)
+        elif(key == 'a'):
+            left_motor.setVelocity(-VELOCITY)
+            right_motor.setVelocity(VELOCITY)
+        elif(key == 'd'):
+            left_motor.setVelocity(VELOCITY)
+            right_motor.setVelocity(-VELOCITY)
+
+if __name__ == "__main__":    
+    #ROBOT INITIALIZATION
+    robot = Robot()
+    timestep = int(robot.getBasicTimeStep())
+
+    #LIDAR INITIALIZATION
+    lidar = robot.getLidar("lidar")
+    lidar.enable(timestep)
+    ranges_str = "1.13114178 0.85820043 0.57785118 0.43461093 0.38639969 0.31585345 0.2667459 0.23062678 0.21593061 0.19141567 0.17178488 0.15571462 0.14872716 0.13643947 0.12597121 0.11696267"
+    RANGES = [float(i) for i in ranges_str.split(' ')]
+    SIZES = (16, 512)
+    EPSILON = 0.6
+
+    #GRAPHER INITIALIZATION
+    DISPLAY_SIZE = (1024, 1024)
+    DISPLAY_SCALING_FACTOR = 1024/RANGES[0]
+    display = robot.getDisplay("extra_display")
+    grapher = Grapher(display)
+    colors = [0xFF0000, 0x800000, 0xFFFF00, 0x808000, 0x00FF00,0x008000, 0x00FFFF, 0x008080, 0x0000FF, 0x000080, 0xFF00FF, 0x800080]
+    
+    #MOTOR INITIALIZATION
+    VELOCITY = 60
+    left_motor = robot.getMotor("left motor")
+    right_motor = robot.getMotor("right motor")
+    left_motor.setPosition(float('inf'))
+    right_motor.setPosition(float('inf'))
+    left_motor.setVelocity(0)
+    right_motor.setVelocity(0)
+    
+    #KEYBOARD INITIALIZATION
+    # keyboard = Keyboard()
+    # keyboard.enable(timestep)
+    # print("Select the 3D window and control the robot using the W, A, S, D keys.")
+
+    #NEIGHBOURS DETECTION INITIALIZATION
+    DELTA_THETA = 0.1
+    DELTA_R = 0.02
+
+    while robot.step(timestep) != -1:
+        theta_data_aligned = get_theta_data_aligned(lidar, SIZES, RANGES, EPSILON) #To read and filter the LIDAR data
+        theta_data_colored = get_theta_data_colored(theta_data_aligned, DELTA_THETA, DELTA_R) #To categorize the LIDAR points into groups
+        neighbours_positions_x, neighbours_positions_y = get_neighbours(theta_data_colored) #To get the x, y of the neighbours
+        
+        swarm_control(neighbours_positions_x, neighbours_positions_y, left_motor, right_motor, VELOCITY)
+        #To control the behaviour of the robot based on the relative positions of its neighbours.
+        
+        # teleoperation(keyboard, left_motor, right_motor, VELOCITY)
+        
